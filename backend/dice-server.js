3c00c2bbd58efd14b254f61eedfb30d2a4e2e978
Sl0wj0e42 <91666405+Sl0wj0e42@users.noreply.github.com>
2024-12-12 02:55:22 +0000

    the dice roll works within a seprate roll folder

--- backend/dice-server.js
+++ backend/dice-server.js
@@ -9,6 +9,32 @@ app.get('/', (req, res) => {
     res.send('Dice Number is: ' + diceNumber.toString());
 });
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get('/dicepages', (req, res) => {
+    let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+    let diceNumber = diceRoll.toString();
+
+    //<post verible name> = {<.ejs Side>: <server side>}
+    let sendDice = {"numberResponse" : diceNumber};
+
+    //res.render(<.ejs page name>, <post verible name>)
+    res.render("dicepages", sendDice);
+});
+
+//Added for testing
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.get("/page", function(request, response) {
+    let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+    let diceNumber = diceRoll.toString();
+    let pageData = {"msg": diceNumber};
+    response.render("page", pageData);
+});
+
 app.listen(port, () => {
     console.log('Running on port: ' + port);
 });
