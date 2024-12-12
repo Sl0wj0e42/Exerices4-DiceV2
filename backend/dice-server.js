56de33ddfdffe652a4c590f6479b3bb7b9b6f478
Sl0wj0e42 <91666405+Sl0wj0e42@users.noreply.github.com>
2024-12-12 02:18:24 +0000

    the dice roll works, still havent got view's tho

--- backend/dice-server.js
+++ backend/dice-server.js
@@ -1,9 +1,12 @@
 let port = 8080;
 let express = require('express');
 let app = express();
+let path = require('path');
 
 app.get('/', (req, res) => {
-    res.send('Hello there!!');
+    diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    res.send('Dice Number is: ' + diceNumber.toString());
 });
 
 app.listen(port, () => {
